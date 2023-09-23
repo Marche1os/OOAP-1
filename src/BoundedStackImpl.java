0dfbcc46a3e2c5808e762a799c8eeedb04a8af56
marche1os <kolpmark@yandex.ru>
2023-09-23 09:49:56 +0300

    BoundedStack impl

--- src/BoundedStackImpl.java
+++ src/BoundedStackImpl.java
@@ -1,27 +1,40 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class BoundedStackImpl<T> {
-    private static final int DEFAULT_CAPACITY = 32;
+abstract class BoundedStack<T> {
+    static final int DEFAULT_CAPACITY = 32;
+    static final int INIT_OK = 0;
+    static final int INIT_INVALID_CAPACITY = 1;
+    static final int PUSH_OK = 0;
+    static final int PUSH_FILLED = 1;
+    static final int PUSH_NULL = 2;
+    static final int PEEK_OK = 0;
+    static final int PEEK_EMPTY_STACK = 1;
+    static final int PEEK_NULL = 2;
+    static final int POP_OK = 0;
+    static final int POP_EMPTY_STACK = 1;
+    static final int POP_NULL = 2;
+
+    protected int initStatus = INIT_INVALID_CAPACITY;
+    protected int pushStatus = PUSH_NULL;
+    protected int peekStatus = PEEK_NULL;
+    protected int popStatus = POP_NULL;
+    protected int capacity;
+
+    abstract void push(final T value);
+
+    abstract T peek();
+
+    abstract T pop();
+
+    abstract void clear();
+
+    abstract public int size();
+}
+
+public class BoundedStackImpl<T> extends BoundedStack<T> {
 
     private List<T> stack;
-    private int initStatus = INIT_INVALID_ARGUMENT;
-    private int pushStatus = PUSH_NULL;
-    private int peekStatus = PEEK_NULL;
-    private int popStatus = POP_NULL;
-    private final int capacity;
-
-    public static final int INIT_OK = 0;
-    public static final int INIT_INVALID_ARGUMENT = 1;
-    public static final int PUSH_OK = 0;
-    public static final int PUSH_FILLED = 1;
-    public static final int PUSH_NULL = 2;
-    public static final int PEEK_OK = 0;
-    public static final int PEEK_EMPTY_STACK = 1;
-    public static final int PEEK_NULL = 2;
-    public static final int POP_OK = 0;
-    public static final int POP_EMPTY_STACK = 1;
-    public static final int POP_NULL = 2;
 
     public BoundedStackImpl() {
         this(DEFAULT_CAPACITY);
@@ -32,11 +45,14 @@ public class BoundedStackImpl<T> {
         if (capacity >= 1) {
             stack = new ArrayList<>(capacity);
             initStatus = INIT_OK;
+            return;
         }
+        initStatus = INIT_INVALID_CAPACITY;
     }
 
+    @Override
     public void push(final T value) {
-        if (stack.size() >= capacity) {
+        if (stack.size() >= capacity || initStatus == INIT_INVALID_CAPACITY) {
             pushStatus = PUSH_FILLED;
             return;
         }
@@ -44,6 +60,7 @@ public class BoundedStackImpl<T> {
         pushStatus = PUSH_OK;
     }
 
+    @Override
     public T peek() {
         if (stack.isEmpty()) {
             peekStatus = PEEK_EMPTY_STACK;
@@ -56,6 +73,7 @@ public class BoundedStackImpl<T> {
         return value;
     }
 
+    @Override
     public T pop() {
         if (stack.isEmpty()) {
             popStatus = POP_EMPTY_STACK;
@@ -68,14 +86,18 @@ public class BoundedStackImpl<T> {
         return value;
     }
 
+    @Override
     public void clear() {
-        stack = new ArrayList<>(capacity);
-        initStatus = INIT_OK;
+        if (capacity >= 1) {
+            stack = new ArrayList<>(capacity);
+            initStatus = INIT_OK;
+        }
         pushStatus = PUSH_NULL;
         peekStatus = PEEK_NULL;
         popStatus = POP_NULL;
     }
 
+    @Override
     public int size() {
         return stack.size();
     }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.Map;
+
 public class Main {
+    private static final Map<Integer, String> humanStatuses = Map.of(
+            0, "OK",
+            1, "FAILURE",
+            2, "NULL"
+    );
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final BoundedStackImpl<Integer> stack = new BoundedStackImpl<>(10);
+
+        printStatuses(stack);
+        stack.peek();
+
+        for (int i = 0; i < 35; i++) {
+            stack.push(i);
+            System.out.println("adding index " + i + ": operation" + humanStatuses.get(stack.getPushStatus()));
+        }
+
+        int lastValue = stack.pop();
+
+        System.out.println("\nLastValue = " + lastValue);
+
+        lastValue = stack.peek();
+
+        System.out.println("\nLastValue = " + lastValue);
+
+        lastValue = stack.pop();
+
+        System.out.println("\nLastValue = " + lastValue);
+
+//        printStatuses(stack);
+    }
+
+    public static void printStatuses(final BoundedStackImpl stack) {
+        System.out.println("\ninit: " + humanStatuses.get(stack.getInitStatus()) +
+                "\npeek = " + humanStatuses.get(stack.getPeekStatus()) +
+                "\npush = " + humanStatuses.get(stack.getPushStatus()) +
+                "\npop = " + humanStatuses.get(stack.getPopStatus())
+        );
     }
 }
